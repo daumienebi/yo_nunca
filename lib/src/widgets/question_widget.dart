329323a05522caa13a8b485321d84ed57a4be53f
daumienebi <ddsakpa@gmail.com>
2022-11-17 18:19:31 +0100

    minor changes + error page when a category has no questions

--- lib/src/widgets/question_widget.dart
+++ lib/src/widgets/question_widget.dart
@@ -69,28 +69,7 @@ class _QuestionWidgetState extends State<QuestionWidget> {
           builder: (_,AsyncSnapshot snapshot){
         List<Widget> test = [];
         if(snapshot.hasData){
-          test = <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    //if data is available then display the questions, else display the error page
-                    snapshot.data != null && snapshot.data.length > 0 ?
-                    _questionTextWidget(snapshot.data[_currentndex].description) : _noQuestionWidget(),
-                    SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        _prevQstButton(),
-                        SizedBox(width: 25),
-                        _nextQstButton(snapshot.data.length)
-                      ],
-                    )
-                  ]
-              ),
-            ),
-          ];
+          test = snapshot.data != null && snapshot.data.length > 0 ? _questionWidget(snapshot) : _noQuestionWidget();
         }else if (snapshot.hasError) {
           test = <Widget>[
             const Icon(
@@ -126,23 +105,79 @@ class _QuestionWidgetState extends State<QuestionWidget> {
     );
   }
 
+  _questionWidget(AsyncSnapshot snapshot){
+    //displays this widget when data is received
+    return <Widget>[
+          Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //if data is available then display the questions, else display the error page
+            snapshot.data != null && snapshot.data.length > 0 ?
+            _questionTextWidget(snapshot.data[_currentndex].description) : _noQuestionWidget(),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _prevQstButton(),
+                SizedBox(width: 25),
+                _nextQstButton(snapshot.data.length)
+              ],
+            )
+          ]
+      )
+    ];
+  }
   _noQuestionWidget(){
-    String msg = 'No existen preguntas para esta categoría. Ve a -> "Mis Categorias" para gestionar la categorías creadas';
-    return Center(
-
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.error_outline,
-            color: Colors.grey,
-            size: 60,
+    //displays this widget when no data is received or when the category has
+    //no questions
+    String errorMessage = 'No hay preguntas para esta categoría, ve a las categorias para añadirlas.';
+    return <Widget>[
+      Container(
+        height: 250,
+        width: 300,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.error_outline,
+                color: Colors.grey,
+                size: 60,
+              ),
+              Text(errorMessage,textAlign: TextAlign.center,style: TextStyle(
+                fontSize: 16,
+              ),),
+            ],
           ),
-          Text(msg,textAlign: TextAlign.center,style: TextStyle(
-            fontSize: 15,
-          ),),
-        ],
+        ),
       ),
+      SizedBox(height: 20,),
+      _goToCategoriesBtn()
+
+
+    ];
+  }
+
+  Widget _goToCategoriesBtn() {
+    return ElevatedButton(
+      onPressed: () {
+        print("TODO: Go to categories");
+      },
+      child: Text('Ir a Categorias'),
+      style: TextButton.styleFrom(backgroundColor: Colors.greenAccent),
     );
   }
 
