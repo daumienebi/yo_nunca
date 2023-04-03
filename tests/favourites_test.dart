7810a82fa92854b3ffb6cf327650048df1d77652
daumienebi <ddsakpa@gmail.com>
2023-04-03 23:41:23 +0200

    adding the fav test again

---
+++ tests/favourites_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:yo_nunca/src/models/category.dart';
+import 'package:yo_nunca/src/models/question.dart';
+import 'package:yo_nunca/src/providers/providers.dart';
+
+import 'test_utils.dart';
+
+void main(){
+  TestWidgetsFlutterBinding.ensureInitialized();
+  // Set up the sqflite_common_ffi for flutter test
+  // Essential step, if not the tests will not run
+  TestUtils.sqlFliteSetUpForTest();
+
+  group("Favourites test", (){
+    QuestionProvider questionProvider = QuestionProvider();
+    CategoryProvider categoryProvider = CategoryProvider();
+
+    // Test 1
+    test("Add a question to favourites", () async{
+      int categoryId,questionId;
+
+      //Add a category for the new question
+      Category category = Category(description: "Test Category X",imageRoute: "");
+      categoryId = await categoryProvider.addCategory(category);
+
+      Question question = Question(
+          categoryId: categoryId,
+          description: "Yo nunca hice un test",
+          isFavourite: false
+      );
+      questionId = await questionProvider.addQuestion(question);
+      question.id = questionId;
+      //int rows = await questionProvider.addToFavourites(question);
+      questionProvider.getFavouriteQuestions();
+      expect(questionProvider.favouriteQuestions.length, 1);
+      //Incorrect test, look into it
+    });
+
+  });
+}
